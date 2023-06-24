a320810eee1910ad03cc3488c64456976d02d40a
andrii-pozniak <andriipozniak@gmail.com>
2023-06-24 17:38:51 +0300

    change connection

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 
-const MONGODB_URL =
-  "mongodb+srv://Andrii:rZx3nn90OEUshYC9@cluster0.hrdghsu.mongodb.net/tasks_reader";
+// const MONGODB_URL =
+//   "mongodb+srv://Andrii:rZx3nn90OEUshYC9@cluster0.hrdghsu.mongodb.net/tasks_reader";
 
 const connectMongo = async () => {
   try {
-    await mongoose.connect(MONGODB_URL, {
+    await mongoose.connect(process.env.MONGO_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
